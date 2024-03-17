544bf00fd0ea6a279683d267c3874cc7f89b0814
Joshua Ssemwanga <jssem001@plattsburgh.edu>
2024-03-17 13:13:57 +0300

    adding array functions

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+let cats =["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+    let copyCats = cats.slice();
+    copyCats.push(name);
+    return copyCats;
+}
+function prependCat(name){
+    let copyCats = cats.slice();
+    copyCats.unshift(name);
+    return copyCats;
+}
+function removeLastCat(){
+    let copyCats = cats.slice();
+    copyCats.pop();
+    return copyCats;
+}
+function removeFirstCat(){
+    let copyCats = cats.slice();
+    copyCats.shift();
+    return copyCats;
+}
